3b34c5603bbf5865c23a303bcdadb7837fe7464a
mrayhanb@student.telkomuniversity.ac.id <m.rayhanbilhaqi@gmail.com>
2021-10-01 18:23:06 +0700

    HelloWorld.java

---
+++ Jurnal02/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Nama : M. Rayhan Bilhaqi");
+		System.out.println("Nim : 6706200031");
+		System.out.println("Kelas : 44-04");
+	}
+}
